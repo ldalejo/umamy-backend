9ee0e0aa5714042666261a34d88a2e0db73dc03d
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-02-19 12:09:42 +0100

    Creado seeder para las categorias de los productos

---
+++ database/seeders/Categoria.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Categoria extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Bebidas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Entrantes',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Hamburguesas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Pizzas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Carnes',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Postres',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
