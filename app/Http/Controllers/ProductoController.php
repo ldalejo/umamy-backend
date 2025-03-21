86ca51f0fdaf85d879b4623af541ee3195cdd01d
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-21 02:35:00 +0100

    Creado end-point para guardar un producto nuevo

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductoCollection;
 use App\Models\Producto;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductoController extends Controller
 {
@@ -19,9 +21,54 @@ class ProductoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function anadirProducto(Request $request)
     {
-        //
+        // Validamos los datos del producto
+        //$data = $request->validated();
+
+        // Buscamos el nombre de la categoria
+        $nombreCategoria = Categoria::where('id', $request->categoria_id)->first()->nombre;
+        $nombreCategoria = strtolower($nombreCategoria);
+
+        // Buscamos la extension de la imagen
+        $extensionImagen = $request->file('imagen')->getClientOriginalExtension();
+
+        // Cambiamos el nombre del producto para guardar la imagen
+        $nombreImagen = str_replace(' ', '_', $request->nombre);
+        $nombreImagen = "$nombreImagen.$extensionImagen";
+        
+        // Iniciar la transacción
+        DB::beginTransaction();
+
+        try {
+            // Guardamos la ruta de la imagen
+            $rutaImagen = $request->hasFile('imagen')
+            ? $request->file('imagen')->storeAs($nombreCategoria, $nombreImagen, 'public') 
+            : null;
+
+            // Almacenamos un nuevo producto
+            $producto = new Producto();
+            $producto->categoria_id = $request->categoria_id;
+            $producto->nombre = $request->nombre;
+            $producto->precio = $request->precio;
+            $producto->imagen = $rutaImagen;
+            $producto->save();
+
+            DB::commit();
+            return response()->json([
+                'message' => 'Producto almacenado.'
+            ]);
+
+        } catch (\Exception $e) {
+            // Si hay algún error, revertimos la transacción
+            DB::rollBack();
+    
+            // Devuelves el error en la respuesta
+            return response()->json([
+                'message' => 'Error al guardar el producto.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::middleware('auth:sanctum')->group(function() {
     // Rutas Productos
     Route::get('/productos', [ProductoController::class, 'index']);
     Route::get('/productos-disponibles', [ProductoController::class, 'productosDisponibles']);
+    Route::post('/producto/anadir-producto', [ProductoController::class, 'anadirProducto']);
     Route::put('/productos/actualizar-producto/{producto}', [ProductoController::class, 'actualizarProducto']);
 
     // Rutas Usuario
